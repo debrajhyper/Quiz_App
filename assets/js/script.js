f49feb952b054a141e8c2d9c4eeac32ddd9f9683
Debraj Karmakar <64654268+debrajhyper@users.noreply.github.com>
2021-01-22 12:11:39 +0530

    update 0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,18 +123,13 @@ function optionSelected(answer){
         userScore += 1; //upgrading score value with 1
         answer.classList.add("correct"); //adding green color to correct selected option
         answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
-        console.log("Correct Answer");
-        console.log("Your correct answers = " + userScore);
     }else{
         answer.classList.add("incorrect"); //adding red color to correct selected option
         answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
-        console.log("Wrong Answer");
-
         for(i=0; i < allOptions; i++){
             if(option_list.children[i].textContent == correcAns){ //if there is an option which is matched to an array answer 
                 option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
                 option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
-                console.log("Auto selected correct answer.");
             }
         }
     }
@@ -155,13 +150,11 @@ function showResult(){
         //   confetti.stop();
         const startit = () => {
             setTimeout(function () {
-            console.log("start");
             confetti.start();
             },);
         };
         const stopit = () => {
         setTimeout(function () {
-            console.log("stop");
             confetti.stop();
             }, 4000);
         };
